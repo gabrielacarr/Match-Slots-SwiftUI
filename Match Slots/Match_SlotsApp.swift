382d6ca6ee5bf6c2bdcc95ddf7dbc437a856ce6f
gabriela <24256452+gabrielacarr@users.noreply.github.com>
2021-06-23 16:32:26 -0400

    Initial Commit

---
+++ Match Slots/Match_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Match_SlotsApp.swift
+//  Match Slots
+//
+//  Created by Brie New on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Match_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
